fca48cdb1aac1fae167b20eb25fea6ea9f770dc9
isHuangXin <597934012@qq.com>
2022-10-23 23:49:25 +0800

    drop-table work

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -171,7 +171,9 @@ void ExecuteStage::handle_request(common::StageEvent *event)
       do_desc_table(sql_event);
     } break;
 
-    case SCF_DROP_TABLE:
+    case SCF_DROP_TABLE: {
+      do_drop_table(sql_event);
+    } break;
     case SCF_DROP_INDEX:
     case SCF_LOAD_DATA: {
       default_storage_stage_->handle_event(event);
@@ -491,6 +493,20 @@ RC ExecuteStage::do_create_index(SQLStageEvent *sql_event)
 
   RC rc = table->create_index(nullptr, create_index.index_name, create_index.attribute_name);
   sql_event->session_event()->set_response(rc == RC::SUCCESS ? "SUCCESS\n" : "FAILURE\n");
+  return rc;
+}
+
+RC ExecuteStage::do_drop_table(SQLStageEvent *sql_event)
+{
+  const DropTable &drop_table = sql_event->query()->sstr.drop_table;
+  SessionEvent *session_event = sql_event->session_event();
+  Db *db = session_event->session()->get_current_db();
+  RC rc = db->drop_table(drop_table.relation_name);
+  if (rc == RC::SUCCESS) {
+    session_event->set_response("SUCCESS\n");
+  } else {
+    session_event->set_response("FAILURE\n");
+  }
   return rc;
 }
 
--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -120,6 +120,50 @@ RC Table::create(
   return rc;
 }
 
+RC Table::drop(const char *name, const char *base_dir)
+{
+  RC rc = sync();
+  if (nullptr == name || common::is_blank(name)) {
+    LOG_WARN("Name cannot be empty");
+    return RC::INVALID_ARGUMENT;
+  }
+  LOG_INFO("Begin to drop table %s:%s", base_dir, name);
+
+  // drop meta data file
+  std::string table_meta_path = table_meta_file(base_dir, name);
+  if (std::remove(table_meta_path.c_str()) != 0) {
+    LOG_ERROR("Cannot remove meta data of table %s", name);
+    return RC::GENERIC_ERROR;
+  }
+
+  // drop table data file
+  std::string table_data_path = table_data_file(base_dir, name);
+  if (std::remove(table_data_path.c_str()) != 0) {
+    LOG_ERROR("Cannot remove file data of table %s", name);
+    return RC::GENERIC_ERROR;
+  }
+
+   // drop index data file
+  for (int i = 0; i < indexes_.size(); i++) {
+    std::string index_file = table_index_file(base_dir_.c_str(), name, indexes_[i]->index_meta().name());
+    // 也许 dynamic_cast 更好
+    rc = reinterpret_cast<BplusTreeIndex *>(indexes_[i])->close();
+    if (rc != RC::SUCCESS) {
+      LOG_ERROR("Failed to close disk buffer pool of index file. file name=%s", index_file.c_str());
+      return rc;
+    }
+    if (std::remove(index_file.c_str()) == -1) {
+      LOG_ERROR("Failed to drop disk buffer pool of index file. file name=%s", index_file.c_str());
+      return RC::GENERIC_ERROR;
+    }
+    if (rc != RC::SUCCESS) {
+      LOG_ERROR("Failed to drop disk buffer pool of index file. file name=%s", index_file.c_str());
+      return rc;
+    }
+  }
+  return rc;
+}
+
 RC Table::open(const char *meta_file, const char *base_dir, CLogManager *clog_manager)
 {
   // 加载元数据文件
